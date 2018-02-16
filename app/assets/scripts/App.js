5d62072c6298830a4c4faafc05130b25d42a3efc
adminuser <adminuser@localhost.localdomain>
2018-02-16 22:23:40 +0100

    Completed header

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,6 @@
+function person (name, favColor) {
+  console.log('Hello my name is ' + name + ' and my favorite color is ' + favColor + '.');
+}
+
+person('John Doe', 'blue');
+person('Jane Smith', 'green');
