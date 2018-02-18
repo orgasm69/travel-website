fdafd5ba1e6f997dd3cb5bebe2b6f74683578b62
adminuser <adminuser@localhost.localdomain>
2018-02-18 21:46:50 +0100

    Created first js constructor.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,13 @@
-function person (name, favColor) {
-  console.log('Hello my name is ' + name + ' and my favorite color is ' + favColor + '.');
+function Person(fullName, favColor) {
+  this.name = fullName
+  this.favoriteColor = favColor;
+  this.greet = function () {
+    console.log('Hello there, my name is ' + this.name + ' and my favorite color is ' + this.favoriteColor + '.');
+  }
 }
 
-person('John Doe', 'blue');
-person('Jane Smith', 'green');
+var john = new Person('John Doe', 'blue');
+john.greet();
+
+var jane = new Person('Jane Smith', 'green');
+jane.greet();
