4dcc473f117474ba24264614455c0d377374e625
unknown <cichy.shebang@gmail.com>
2018-02-25 22:31:11 +0100

    Reveal on scroll added

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,30 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+  constructor() {
+    this.itemsToReveal = $('.feature-item');
+    this.hideInitialy();
+    this.createWaypoints();
+  }
+
+  hideInitialy() {
+    this.itemsToReveal.addClass('reveal-item');
+  }
+
+  createWaypoints() {
+    this.itemsToReveal.each(function() {
+      var currentItem = this;
+      new Waypoint({
+        element: currentItem,
+        handler: function() {
+          $(currentItem).addClass('reveal-item--is-visible');
+        },
+        offset: '85%'
+      });
+    });
+  }
+
+}
+
+export default RevealOnScroll;
